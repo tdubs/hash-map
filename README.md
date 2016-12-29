1268ba936a70e9a8ffcc7955df48514274b0ed1f
tdubs <tw@leetsys.com>
2016-12-29 16:19:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# hash map tool
+
+ A very simple tool to map cracked passwords back
+ to usernames after you've cracked them. The package
+ includes two example files as examples. Be sure to check
+ them as the formats must be the same!
+ Special note, you must specify the proper heading for the
+ cracked password file. Heading is:
+ hash,password
+
+# Necessary libraries:
+cpan Text::CSV::Hashify
+
+# Example Usage:
+```
+user@linux:/# ././hash.map.pl cracked.txt hashes.txt 
+[+] Opening Pass File cracked.txt
+[+] Opening Original Hash File cracked.txt
+user1:11111,userpass1
+user2:22222,userpass2
+user3:33333,userpass3
+user4:44444,userpass4
+user5:55555,userpass5
+
+user@linux:/# 
+```
---
+++ hash.map.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+#
+# to install module use
+# cpan Text::CSV::Hashify
+#
+# 
+# IMPORTANT ### The first file 'pass_file'
+# is a csv file which must have heading of
+# hash,password
+# these should be the first two values
+#
+$pass_file = $ARGV[0];
+$hash_file = $ARGV[1];
+
+use Text::CSV;
+use Text::CSV::Hashify;
+
+if ( @ARGV != 2 )
+{
+print " -= Hash Map =-\n";
+print "useage: $0 cracked.txt hashes.txt\n";
+print "cracked.txt: hashes and clear text password\n";
+print "hashes.txt: original userid and hash file\n";
+print "\n**Special Note**\ncracked.txt must have heading of:\n";
+print "hash:password\n\n";
+exit;
+}
+
+print "[+] Opening Pass File $pass_file\n";
+print "[+] Opening Original Hash File $pass_file\n";
+
+$obj = Text::CSV::Hashify->new( {
+ sep_char => ':',
+ file => $pass_file,
+ key => 'hash' } );
+
+$hash_ref = $obj->all;
+ 
+open(my $data, '<', $hash_file) or die "Could not open '$hash_file' $!\n";
+
+# hash_file format
+# USERID:SID:LMHASH:NTLMHASH
+
+# For each line in the Hash file 
+# which is the file USERID:HASH 
+while (my $line = <$data>) {
+
+# if ($line != "" )
+chomp($line);
+  @fields = split ":", $line;
+  $user = $fields[0];
+  $hash = $fields[1];
+
+ #print "[D] Searching for key: $hash\n";
+  # datum( value of key, field )
+  if ( $pass = $obj->datum( $hash, 'password')  )
+  {
+    print "$line,$pass\n";
+  }
+  else
+  {
+   #  print "NOMATCH, $line with $nthash\n";
+   }
+}
+
+
